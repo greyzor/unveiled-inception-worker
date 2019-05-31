1bd24eae08f459698741fb55252b888b3cf159ff
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-06-01 00:32:28 +0200

    remove unnecessary comments.

--- manage.py
+++ manage.py
@@ -19,12 +19,6 @@ def run():
 def test():
     """ Run main application. """
     pytest.main(['-x', 'tests/'])
-    # tests = unittest.TestLoader().discover('tests/')
-    # print('tests: %s' % tests)
-    # result = unittest.TextTestRunner(verbosity=2).run(tests)
-    # if result.wasSuccessful():
-    #     return 0
-    # return 1
 
 if __name__ == '__main__':
     manager.run()
