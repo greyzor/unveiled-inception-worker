1e5eb02091f1d0946784e5f1665670c4bbecd0fd
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-06-01 00:11:48 +0200

    classify image passed into request.data thus return labels and scores.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from config import DEBUG, LOG_FORMAT
 from image_classification import InceptionV3Classifier
 import logging
 import os
+import cv2
+import numpy as np
+import io
+import time
 
 # Load Inceptionv3 classifier in memory
 CLASSIFIER = InceptionV3Classifier(weights='imagenet')
@@ -28,13 +32,23 @@ def create_app():
             "status": "ok"
         })
 
-    @app.route('/api/1/process')
-    def process():
-        """ Root handler. """
+    @app.route('/api/1/classify', methods=['POST'])
+    def classify_image():
+        """ Task handler. """
+        t_start = time.time()
+        stream = io.BytesIO(request.data)
+        res = CLASSIFIER.classify(stream, top=5)
+
+        # convert score from float to str
+        res = [(t[0], t[1], str(t[2])) for t in res[0]]
+
         return jsonify({
-            "status": "processing"
+            "status": "done",
+            "timing": time.time()-t_start,
+            "results": res
         })
     return app
+
 
 if __name__ == '__main__':
     """ Main entrypoint. """
